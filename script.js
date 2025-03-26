33379f48c3c4a36e2ada4a653dc4b935cdceff8a
Mhaque2000 <mahamoodulhaque@gmail.com>
2025-03-26 10:30:38 +0530

    login-reg-page-complete

--- script.js
+++ script.js
@@ -5,14 +5,25 @@ document.addEventListener("DOMContentLoaded", function () {
         // alert("You're in the register page");
         registerForm.addEventListener("submit", function (e) {
             e.preventDefault();
-            
+
             const username = document.getElementById("rgname").value;
             const password = document.getElementById("rgpass").value;
 
-            localStorage.setItem("username", username);
-            localStorage.setItem("password", password);
+            let users = JSON.parse(localStorage.getItem('users')) || []
 
-            // alert("Registered successfully!!");
+            const userExists = users.some(user => user.username === username)
+
+            if(userExists){
+                alert('user is already exits, please choose another username')
+                return
+            }
+
+            users.push({username: username, password: password})
+
+            localStorage.setItem('users',JSON.stringify(users))
+
+            alert('registration is successful')
+
             this.reset(); // Clear the form
         });
     }
@@ -27,23 +38,30 @@ document.addEventListener("DOMContentLoaded", function () {
             const loginUsername = document.getElementById("lname").value;
             const loginPassword = document.getElementById("lpass").value;
 
-            const getUsernameFromLocalStorage = localStorage.getItem("username");
-            const getPasswordFromLocalStorage = localStorage.getItem("password");
+            const users = JSON.parse(localStorage.getItem('users')) || []
 
-            if (loginUsername === getUsernameFromLocalStorage && loginPassword === getPasswordFromLocalStorage) {
+            const validUser = users.find(user => user.username === loginUsername && user.password===loginPassword)
+
+            if (validUser) {
+                localStorage.setItem('loggedInUserName', validUser.username)
                 window.location.href = "./views/welcome.html";
             } else {
-                // alert("Invalid username or password");
+                alert("Invalid username or password");
             }
-
+            
             this.reset();
         });
     }
     const logout = document.getElementById('logout-btn')
-    if(logout){
-        logout.addEventListener('click', function(e){
-            localStorage.clear()
-            window.location.href='/index.html'
+    if (logout) {
+        logout.addEventListener('click', function (e) {
+            // localStorage.clear()
+            localStorage.removeItem('loggedInUserName')
+            window.location.href = '/login.html'
         })
+    }
+    const loggedInUser = document.getElementById('loggedInUser')
+    if(loggedInUser){
+        loggedInUser.innerText = localStorage.getItem('loggedInUserName')
     }
 });
